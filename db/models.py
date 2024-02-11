0f7599c99ca919513788688dd70d24b7d99a1ef0
Vadym Hotko <poipooo600@gmail.com>
2024-02-11 11:36:19 +0000

    added database

---
+++ db/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from .engine import Base
+
+class DBUser(Base):
+    __tablename__ = 'users'
+    id = Column(Integer,primary_key=True,index=True)
+    username = Column(String,unique=True,nullable=False)
+    email = Column(String,unique=True,nullable=False)
+    
+    
+    
